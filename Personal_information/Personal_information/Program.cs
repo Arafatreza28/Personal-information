9a001c7604cfa5f796ab2f5199db61dbf278a864
Arafatreza28 <arafatrezabitm3@gmail.com>
2019-09-10 03:53:16 +0600

    Screenshots of my Code, Input & Output added

---
+++ Personal_information/Personal_information/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Name = " ";
+            String Age = " ";
+            String Address = " ";
+            String Contact = " ";
+
+            Console.WriteLine("Enter your name :");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter your age :");
+            Age = Console.ReadLine();
+            Console.WriteLine("Enter your address :");
+            Address = Console.ReadLine();
+            Console.WriteLine("Enter your contact :");
+            Contact = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("Name :");
+            Console.WriteLine(Name);
+            Console.WriteLine("Age :");
+            Console.WriteLine(Age);
+            Console.WriteLine("Address :");
+            Console.WriteLine(Address);
+            Console.WriteLine("Contact :");
+            Console.WriteLine(Contact);
+            Console.ReadLine();
+        }
+    }
+}
